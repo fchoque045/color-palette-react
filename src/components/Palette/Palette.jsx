2996cb44e319fa909be962b7385cf7ed537f560a
Fabian Choque <fabianchoque28@gmail.com>
2022-09-24 12:03:37 -0300

    routes agregadas

--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,35 +1,36 @@
-import { useContext, useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { FavoritesContext } from '../../context/FavoritesContext';
 import './Palette.css';
 
-const Palette = ({ palette }) => {
+const Palette = ({palette}) => {
 
-    const { id, name, colors, liked } = palette
-    const { favorites, setFavorites } = useContext(FavoritesContext);
-    const [isFavorite, setIsFavorite] = useState(liked);
-  
-    const handleFavorite = () => {
-        setIsFavorite((isFavorite) => !isFavorite);
-    
-        //busco si la paleta ya esta en favoritos
-        const foundIndex = favorites.findIndex(fav => fav.id === id);
-    
-        //para agregar a favoritos
-        if (foundIndex === -1) {
-          setFavorites([...favorites, palette])
-          return
-        }
-    
-        //Quitar de favoritos
-        setFavorites(
-          favorites.filter((fav) => fav.id !== id )//!==
-        );
-      }
-    
+  const { id, name, colors, liked } = palette
+  const { favorites, setFavorites } = useContext(FavoritesContext);
+  const [isFavorite, setIsFavorite] = useState(liked);
 
-  return (
-    <div className='palette-container'>
+  //TODO tienes que modificar el like en la paleta
+  const handleFavorite = () => {
+    setIsFavorite((isFavorite) => !isFavorite);
+
+    //busco si la paleta ya esta en favoritos
+    const foundIndex = favorites.findIndex(fav => fav.id === id);
+
+    //para agregar a favoritos
+    if (foundIndex === -1) {
+      setFavorites([...favorites, palette])
+      return
+    }
+
+    //Quitar de favoritos
+    setFavorites(
+      favorites.filter((fav) => fav.id !== id )//!==
+    );
+  }
+
+  return(
+<div className='palette-container'>
       <div className='palette'>
         <h3>{name}</h3>
         {colors.map((color) => {
@@ -44,15 +45,21 @@ const Palette = ({ palette }) => {
           );
         })}
       </div>
-      <div className='fav'>
-        {liked ? (
-          <FaHeart className='fav heart' onClick={handleFavorite}/>
-        ) : (
-          <FaRegHeart className='fav' onClick={handleFavorite}/>
-        )}
+      <div className='palette-actions'>
+        <div className='fav'>
+          {isFavorite ? (
+            <FaHeart className='fav heart' onClick={handleFavorite}/>
+          ) : (
+            <FaRegHeart className='fav' onClick={handleFavorite}/>
+          )}
+        </div>
+        <Link className='btn-see-more' to={`/palette/${id}`}>
+            Ver más
+        </Link>
       </div>
+
     </div>
-  );
+  )
 }
 
 export default Palette
